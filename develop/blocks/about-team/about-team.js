7997c85aadb057955d474e04c958092e8f751f51
cooltolia <cooltolia@gmail.com>
2018-09-13 00:12:56 +0300

    Закончена страница about

--- develop/blocks/about-team/about-team.js
+++ develop/blocks/about-team/about-team.js
@@ -1,3 +1,44 @@
 ;(function() {
+    $('.about-team__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: true,
+        dots: false,
+        infinite: true,
+        // variableWidth: true,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                // variableWidth: false,
+                // centerMode: true,
+                // centerPadding: '30px'
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 481,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                centerMode: true,
+                centerPadding: '30px',
+                adaptiveHeight: true
+            }
+        }
+    ]
+    });
 
+    $('.about-team__slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        $(currentSlide).addClass('active')
+    });
 })();
