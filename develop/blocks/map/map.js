8247782358851ac98cca3934dabb89dbea8d7162
Anatoliy Kulak <cooltolia@gmail.com>
2018-09-13 20:15:57 +0300

    правки

--- develop/blocks/map/map.js
+++ develop/blocks/map/map.js
@@ -12,21 +12,11 @@
             });
             var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
                 hintContent: 'Пресненская наб., д.8, стр.1',
-                //balloonContent: 'Это красивая метка'
             }, {
-                    // Опции.
-                    // Необходимо указать данный тип макета.
-                    iconLayout: 'default#image',
-                    // Своё изображение иконки метки.
-                    iconImageHref: '../images/map-icon.png',
-                    // Размеры метки.
-                    iconImageSize: [30, 30],
-                    // Смещение левого верхнего угла иконки относительно
-                    // её "ножки" (точки привязки).
-                    iconImageOffset: [-15, -30]
+                    preset: "islands#yellowStretchyIcon",
                 });
 
-            // myMap.geoObjects.add(myPlacemark);
+            myMap.geoObjects.add(myPlacemark);
             myMap.behaviors.disable('scrollZoom');
         });
 
