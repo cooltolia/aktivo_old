c71972cc8984f18e651b7089bf387c1cacf24196
Anatoliy Kulak <cooltolia@gmail.com>
2018-09-13 20:27:02 +0300

    + еще немного правок

--- docs/js/main.js
+++ docs/js/main.js
@@ -1216,105 +1216,7 @@ jQuery(document).ready(function ($) {
      
 
      
-     (function () {
-
-         /** main-nav scripts */
-
      
-
-     
-
-         /** mobile-nav scripts */
-
-         var $mobileNav = $('.mobile-nav'),
-
-             $firstLevelLinks = $('.mobile-nav__link'),
-
-             $secondLevelLinks = $('.mobile-nav__sublink');
-
-     
-
-         $firstLevelLinks.each(function () {
-
-             if ($(this).next('ul').length > 0)
-
-                 $(this).addClass('has-submenu');
-
-         })
-
-     
-
-         $firstLevelLinks.on('click', function (e) {
-
-             var $subMenu = $(this).next();
-
-     
-
-             if ($subMenu.length > 0) {
-
-                 e.preventDefault();
-
-                 e.stopPropagation()
-
-     
-
-                 $subMenu.toggleClass('opened');
-
-                 $(this).addClass('has-submenu');
-
-                 $(this).toggleClass('opened');
-
-     
-
-                 if ($subMenu.hasClass('opened')) {
-
-                     $subMenu.slideDown();
-
-                 } else {
-
-                     $subMenu.slideUp();
-
-                 }
-
-             } else {
-
-                 return;
-
-             }
-
-     
-
-         });
-
-     
-
-         $secondLevelLinks.on('click', function (e) {
-
-             e.preventDefault();
-
-             e.stopPropagation()
-
-             var $thirdMenu = $(this).next();
-
-             $thirdMenu.toggleClass('opened')
-
-     
-
-     
-
-             if ($thirdMenu.hasClass('opened')) {
-
-                 $thirdMenu.slideDown();
-
-             } else {
-
-                 $thirdMenu.slideUp();
-
-             }
-
-         });
-
-     })()
 
      
      ;(function(){
@@ -1345,35 +1247,15 @@ jQuery(document).ready(function ($) {
 
                      hintContent: 'Пресненская наб., д.8, стр.1',
 
-                     //balloonContent: 'Это красивая метка'
-
                  }, {
 
-                         // Опции.
-
-                         // Необходимо указать данный тип макета.
-
-                         iconLayout: 'default#image',
-
-                         // Своё изображение иконки метки.
-
-                         iconImageHref: '../images/map-icon.png',
-
-                         // Размеры метки.
-
-                         iconImageSize: [30, 30],
-
-                         // Смещение левого верхнего угла иконки относительно
-
-                         // её "ножки" (точки привязки).
-
-                         iconImageOffset: [-15, -30]
+                         preset: "islands#yellowStretchyIcon",
 
                      });
 
      
 
-                 // myMap.geoObjects.add(myPlacemark);
+                 myMap.geoObjects.add(myPlacemark);
 
                  myMap.behaviors.disable('scrollZoom');
 
