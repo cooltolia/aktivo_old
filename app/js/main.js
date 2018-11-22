4aeaa4d12475c60b64d24eab6c3da84d5bad7a90
Anatoliy Kulak <cooltolia@gmail.com>
2018-11-22 21:06:11 +0300

    partner page

--- app/js/main.js
+++ app/js/main.js
@@ -2955,6 +2955,145 @@ jQuery(document).ready(function ($) {
      
 
      
+     // ;(function() {
+
+     
+
+     // })();
+
+     
+     // ;(function() {
+
+     
+
+     // })();
+
+     
+     // ;(function() {
+
+     
+
+     // })();
+
+     
+     
+
+     
+     ;(function() {
+
+         var slider = $('.partner-slider__slider');
+
+     
+
+         slider.on('init', function (event, slick, currentSlide) {
+
+             var current = $(slick.$slides[slick.currentSlide]);
+
+             var next = current.next();
+
+             var prev = current.prev();
+
+     
+
+             prev.addClass('slick-sprev');
+
+             next.addClass('slick-snext');
+
+             current.removeClass('slick-snext').removeClass('slick-sprev');
+
+     
+
+             slick.$prev = prev;
+
+             slick.$next = next;
+
+     
+
+         }).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+
+             var current = $(slick.$slides[nextSlide]);
+
+             slick.$prev.removeClass('slick-sprev');
+
+             slick.$next.removeClass('slick-snext');
+
+     
+
+             var next = current.next();
+
+             var prev = current.prev();
+
+     
+
+             prev.prev();
+
+             prev.next();
+
+             prev.addClass('slick-sprev');
+
+             next.addClass('slick-snext');
+
+             slick.$prev = prev;
+
+             slick.$next = next;
+
+     
+
+             current.removeClass('slick-next').removeClass('slick-sprev');
+
+         });
+
+     
+
+         slider.slick({
+
+             speed: 500,
+
+             arrows: true,
+
+             dots: false,
+
+             focusOnSelect: true,
+
+             infinite: true,
+
+             centerMode: true,
+
+             slidesPerRow: 1,
+
+             slidesToShow: 1,
+
+             slidesToScroll: 1,
+
+             centerPadding: '0',
+
+             swipe: true,
+
+             autoHeight: true,
+
+             // autoplay: true,
+
+             // autoplaySpeed: 3000,
+
+         });
+
+     })();
+
+     
+     ;(function() {
+
+     
+
+         new WOW().init();
+
+     
+
+     })();
+
+     
+     
+
+     
      
 
      
